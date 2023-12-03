d034e4ce9ff2cb2dcac7aa738f407d0d84912251
Luis Arroyo Tapia <luisenriquearroyo07@gmail.com>
2023-12-03 12:30:13 -0500

    cambios de urls

---
+++ owner/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('owner_list/', views.owner_list, name='owner_list')
+
+]
+
+
+
--- owner/views.py
+++ owner/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+def owner_list(request):
+    data_context = {
+        'nombre': 'Katty Paredes',
+        'edad': 20,
+        'dni': '54366417',
+        'pais': 'Peru',
+        'vigente': False,
+    }
+    return render(request,'owner_list.html', context ={'data': data_context})
